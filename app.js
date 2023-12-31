b7b5c6cea1cac76376a459044b90ff4335343a89
xoboom <tdub.walton3@gmail.com>
2023-12-30 17:31:37 -0800

    idk

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.static('public'));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
